16ea2b4dc085539805bf2fa9a71cd4ba70f835d9
Gustavo <117860312+SenhorGustavoKK@users.noreply.github.com>
2023-11-30 13:49:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-alert('página com vírus');
-<button onclick="alert('Pom')" class="tecla tecla_pom">Pom</button>
-<audio controls="" src="sounds/keyq.wav" id="som_tecla_pom"></audio>
+alert('Essa página contém vírus');
+
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){document.querySelector('#som_tecla_clap').play();}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim(){document.querySelector('#som_tecla_tim').play();}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){document.querySelector('#som_tecla_puff').play();}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){document.querySelector('#som_tecla_splash').play();}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){document.querySelector('#som_tecla_toim').play();}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){document.querySelector('#som_tecla_psh').play();}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){document.querySelector('#som_tecla_tic').play();}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom(){document.querySelector('#som_tecla_tom').play();}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+const ListadeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+while (contador < ListadeTeclas.length) {
+    ListadeTeclas[0] .onclick = tocaSomPom;
+    contador = contador +1;
+    console.log(contador)
+}
